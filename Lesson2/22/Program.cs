cca3f7f71c5c6d7fe11c8f3361709b7e96e27d22
Radif <radif.suleymanov.98@gmail.com>
2022-11-20 18:35:22 +0300

    22

---
+++ Lesson2/22/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+Console.Write("введите число:");
+int n=Convert.ToInt32(Console.ReadLine());
+
+if (n%10==n/10000 || n/10%10==n/1000)
+    {
+        System.Console.WriteLine($"{n} - данное число является полиндромом");
+    }
+
+else 
+    {
+        System.Console.WriteLine($"{n} - данное число не является полиндромом");
+    }    
