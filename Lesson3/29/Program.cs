6161d83e334199ad966007fd07385911c294cb6c
Radif <radif.suleymanov.98@gmail.com>
2022-11-22 01:14:15 +0300

    29

---
+++ Lesson3/29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Подсчитать сумму цифр в числе. Сделать подпрограмму.
+int Summa(int Number)
+{
+    Number=Math.Abs(Number);
+    int Sum=Number%10;
+    if (Number==0) Sum=0;
+    while (Number>0)
+    {
+        Number=Number/10;
+        Sum=Sum+Number%10;
+    }
+    return Sum;
+}
+
+Console.Write("Введите N:");
+int N=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Summa(N));
