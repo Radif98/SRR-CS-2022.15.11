e1bb74c320fbe435063e2c9e2fa21df30fc37d57
Radif <radif.suleymanov.98@gmail.com>
2022-11-21 23:22:12 +0300

    a.i.

---
+++ Lesson3/24/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+System.Console.Write("введитe N: ");
+int n=Convert.ToInt32(Console.ReadLine()); 
+for (int i = 1; i <= n; i++)
+    {
+        i=i*i;
+    
+        Console.WriteLine($"{i}");
+    } 
