02053b632cb7962b7f1b7ca509b4c67225b84098
Radif <radif.suleymanov.98@gmail.com>
2022-11-22 00:26:26 +0300

    24

--- Lesson3/24/Program.cs
+++ Lesson3/24/Program.cs
@@ -1,9 +1,14 @@
 ﻿// Вывести на экран таблицу квадратов чисел от 1 до N
-System.Console.Write("введитe N: ");
-int n=Convert.ToInt32(Console.ReadLine()); 
-for (int i = 1; i <= n; i++)
-    {
-        i=i*i;
+// Задача на цикл, по этому не стал усложнять подпрограммой
+ 
+Console.Write("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine()); 
+ 
+Console.WriteLine("Число" + "\t" + "Квадрат");
+{
+for(int i = 1; i <= N; i++)
+Console.WriteLine(i + "\t" + i*i);
+}
+
+  
     
-        Console.WriteLine($"{i}");
-    } 
