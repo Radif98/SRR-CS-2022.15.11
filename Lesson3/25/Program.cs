8d88f64201e67c309c1b1ded867a90d064ade487
Radif <radif.suleymanov.98@gmail.com>
2022-11-22 00:35:02 +0300

    25

---
+++ Lesson3/25/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+// Задача цикл, нет смысла усложнять подпрограммой 
+Console.Write("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine()); 
+ 
+Console.WriteLine("Число" + "\t" + "Куб");
+{
+for(int i = 1; i <= N; i++)
+Console.WriteLine(i + "\t" + i*i*i);
+}
