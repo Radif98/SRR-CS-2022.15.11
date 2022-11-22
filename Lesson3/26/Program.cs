7bcdfae9ee69de96e34a4becafa5e6fd37a1dddd
Radif <radif.suleymanov.98@gmail.com>
2022-11-22 03:24:42 +0300

    26

---
+++ Lesson3/26/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите A:");
+int A = Convert.ToInt32(Console.ReadLine()); 
+ 
+{
+int sum=0;    
+for(int i = 1; i <= A; i++)
+    {
+        sum=sum+i;
+    }
+Console.WriteLine($"сумма чисел= "+ sum);
+}
+
