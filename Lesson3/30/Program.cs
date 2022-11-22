7880c98c187b055453fa8647875c2d129a616671
Radif <radif.suleymanov.98@gmail.com>
2022-11-22 04:48:07 +0300

    30

---
+++ Lesson3/30/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите N:");
+double N=Convert.ToInt32(Console.ReadLine());
+
+
+double Op(double Number)
+{
+    double op=1;    
+    for(int i = 1; i <= N; i++)
+    {
+        op=op*i;
+    }
+    return op;
+}
+
+
+System.Console.WriteLine(Op(N));
