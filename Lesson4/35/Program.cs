1a7517bc5123ff0f60369df310964f922bb9444b
Radif <radif.suleymanov.98@gmail.com>
2022-11-25 00:49:15 +0300

    Pause

---
+++ Lesson4/35/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу замены элементов массива на противоположные
+int[] RandomIntArray(int size=10,int min=0,int max=50)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max+1);
+    return a;
+}
+/*
+int ElementReplacement(int[] a)
+{
+    for(int i; i<10; i++)
+        a=a[i]*-1;
+    return a;
+}
+*/
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+int[] a=RandomIntArray();
+Print(a);
+System.Console.WriteLine();
+
+
+
