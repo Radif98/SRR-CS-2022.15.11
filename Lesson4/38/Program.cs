2b90f0df9fcc416a478779183ff1d6c74741fb80
Radif <radif.suleymanov.98@gmail.com>
2022-11-27 20:20:33 +0300

    40

---
+++ Lesson4/38/Program.cs
@@ -0,0 +1,32 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] RandomIntArray(int size=123,int min=0,int max=1000)
+
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max+1);
+    return a;
+}
+
+int Count(int[] a)
+{
+    int count=0;
+    for(int i=0;i<a.Length;i++)
+        if (a[i]>11 & a[i]<100)
+            count++;
+    return count;
+}
+
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+int[] a=RandomIntArray();
+Print(a);
+System.Console.WriteLine();
+System.Console.WriteLine("количество элементов из отрезка [10,99]= " + Count(a));
---
+++ Lesson4/40/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задан массив a из N элементов. Заполнить массив случайными числами от 1 до 10. 
+//Создайте массив, который является произведением пар чисел в одномерном массиве a.
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+int[] RandomIntArray(int size=10,int min=1,int max=10)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max+1);
+    return a;
+}
+/*
+void NewIntArray(int[] a)
+{
+    int size=10;
+    for (int i = 0; i < a.Length; ++i)
+        a[i] = a[i]*a[size-i];
+}
+*/
+ 
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+ 
+int[] a=RandomIntArray();
+Console.Write("Исходный массив:");
+      Print(a);
+      //NewIntArray(a);
+      System.Console.WriteLine();
+Console.Write("Новый массив:");
+      Print(a);
