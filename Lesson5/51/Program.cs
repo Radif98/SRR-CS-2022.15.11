625990ea55fc61a959f671f816a6bce4905d8344
Radif <radif.suleymanov.98@gmail.com>
2022-11-29 22:34:59 +0300

    52

---
+++ Lesson5/51/Program.cs
@@ -0,0 +1,31 @@
+﻿// С клавиатуры вводится число N. Затем вводятся N чисел. 
+//Определить сколько чисел больше 0 введено с клавиатуры
+//массивы - это объекты
+int[] Array(int n)
+{
+int[] t=new int[n];//создаем массив, то есть в t помещаем адрес массива
+//заполнить массив
+for(int i=0;i<n;i++)
+    t[i]=Convert.ToInt32(Console.ReadLine());
+    return t;
+}
+
+int Count(int[] t)
+{
+    int count=0;
+    for(int i=0; i<t.Length; i++)
+        if (t[i]>0) count++;
+    return count;
+}
+void Print(int[] t)
+{
+    for(int i=0;i<t.Length;i++)
+        System.Console.Write($"{t[i],5}");
+}
+System.Console.Write("Введите число N:");
+int n=Convert.ToInt32(Console.ReadLine());
+int[] a=Array(n);
+Print(a);
+Count(a);
+System.Console.WriteLine();
+System.Console.WriteLine($"Числа больше 0= {Count(a)}");
