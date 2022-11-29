494088de27be060abbf5db7f50c23d1ba258cdae
Radif <radif.suleymanov.98@gmail.com>
2022-11-30 01:22:42 +0300

    55

---
+++ Lesson5/55/Program.cs
@@ -0,0 +1,36 @@
+﻿// Написать программу масштабирования фигуры
+// Рассмотрим вариант для фигуры из 4 координат 
+
+int[] Array(int p)
+{
+int[] t=new int[p];
+for(int i=0;i<p;i++)
+    t[i]=Convert.ToInt32(Console.ReadLine());
+    return t; 
+}
+
+void Print(int[] t)
+{
+    for(int i=0;i<t.Length;i++)
+        System.Console.WriteLine($"x\ny{t[i],10}");
+
+           
+}
+
+/*void PrintXY(int[] t)
+{
+    for(int i=0;i<t.Length;i++)
+        System.Console.WriteLine(" x \n y");
+
+}
+*/
+System.Console.Write("Введите количество координат N:");
+int n=Convert.ToInt32(Console.ReadLine());
+int p=n*2;
+/*System.Console.Write("Введите масштаб фигуры:");
+int k=Convert.ToInt32(Console.ReadLine());
+double x=x*k;
+double y=y*k;*/
+int[] a=Array(n);
+//PrintXY(a);
+Print(a);
