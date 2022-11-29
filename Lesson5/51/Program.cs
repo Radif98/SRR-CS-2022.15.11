4125d9d2278ce4291742d8c68ca0b1bb2dbf2cb4
Radif <radif.suleymanov.98@gmail.com>
2022-11-30 00:19:39 +0300

    54

--- Lesson5/51/Program.cs
+++ Lesson5/51/Program.cs
@@ -3,8 +3,7 @@
 //массивы - это объекты
 int[] Array(int n)
 {
-int[] t=new int[n];//создаем массив, то есть в t помещаем адрес массива
-//заполнить массив
+int[] t=new int[n];
 for(int i=0;i<n;i++)
     t[i]=Convert.ToInt32(Console.ReadLine());
     return t;
@@ -17,11 +16,13 @@ int Count(int[] t)
         if (t[i]>0) count++;
     return count;
 }
+
 void Print(int[] t)
 {
     for(int i=0;i<t.Length;i++)
         System.Console.Write($"{t[i],5}");
 }
+
 System.Console.Write("Введите число N:");
 int n=Convert.ToInt32(Console.ReadLine());
 int[] a=Array(n);
---
+++ Lesson5/54/Program.cs
@@ -0,0 +1,22 @@
+﻿// С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. 
+//Принять первые числа равными 0 и 1
+int[] Array(int n)
+{
+int[] t=new int[n];
+for(int i=0;i<2;i++)
+    t[i]=Convert.ToInt32(Console.ReadLine());
+        for(int j=2; j<n; j++)
+            t[j]=t[j-2]+t[j-1];
+    return t;
+}
+
+void Print(int[] t)
+{
+    for(int i=0;i<t.Length;i++)
+        System.Console.Write($"{t[i],5}");
+}
+
+System.Console.Write("Введите размер массива N:");
+int n=Convert.ToInt32(Console.ReadLine());
+int[] a=Array(n);
+Print(a);
