d877f31cf017f248c4a7fc7c5e651684bd062cfe
Radif <radif.suleymanov.98@gmail.com>
2022-12-06 14:26:54 +0300

    60

---
+++ Lesson6/60/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми случайными числами
+int[,] Random2DArray(int m,int n)
+{
+    int[,] a=new int[m,n];
+    Random random=new Random();
+    for(int i=0;i<m;i++)
+         for(int j=0;j<n;j++)
+                a[i,j]=random.Next(10,30);
+    return a;
+}
+
+void Print2DArray(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+        {
+     for(int j=0;j<a.GetLength(1);j++)
+        System.Console.Write($"{a[i,j],4}");
+           System.Console.WriteLine();
+        }
+}
+
+int[,] a=Random2DArray(5,5);
+Print2DArray(a);
