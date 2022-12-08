a87e753c1a92dfb1a746d0307a4862461f0d33d8
Radif <radif.suleymanov.98@gmail.com>
2022-12-08 21:07:18 +0300

    70

---
+++ Lesson7/70/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+void Loop(int N,int i)
+{
+    if (i<=N)
+    {
+        System.Console.Write(i+" ");
+        Loop(N,i+1);
+
+    }    
+}
+
+int N=10;
+Loop(N,1);
