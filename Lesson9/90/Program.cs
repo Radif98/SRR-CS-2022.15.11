90973eaeade5d7673e9e9de52f25b34457cae7a8
Radif <radif.suleymanov.98@gmail.com>
2022-12-14 16:45:55 +0300

    The end

--- Lesson9/90/Program.cs
+++ Lesson9/90/Program.cs
@@ -12,6 +12,58 @@ cd
 1, 7, 0, 1
  
 То есть, 2 бита, потом еще 3 бита, потом еще 3 бита и еще 1 бит. Переводим биты в десятичное представление.
-Какие ошибки могут возникнуть при обработке наборов данных?*/
+Какие ошибки могут возникнуть при обработке наборов данных?
+*/
+ int[] ArrayData(int n)
+{
+    int[] a=new int[n];
+    for(int i=0;i<n;i++) 
+        a[i]=int.Parse(Console.ReadLine());
+    return a;
+}
+
+//int[] BinToDec(int[] a)
+   
+int[] ArrayInfo(int[] c)
+{
+    int[] b=new int[c.Length];
+    for(int i=0;i<c.Length;i++)
+        b[i]=Convert.ToInt32(Math.Log2(c[i]));
+    return b;
+}
+
+void PrintArray(int n, int[] a, int[] b, int[] c)
+{
+    for(int i=0;i<n;i++)
+        {
+        System.Console.Write($"{a[i],4}");
+        }
+    System.Console.WriteLine();
+    for(int i=0;i<n;i++)
+        {
+            System.Console.Write($"{b[i],4}");
+        }
+    System.Console.WriteLine();    
+    for(int i=0;i<n;i++)
+        {
+            System.Console.Write($"{c[i],4}");
+        }
+    
+}
+
+
+
+int n=5;
+Console.WriteLine("Вводите числа BIN");
+int[] a=ArrayData(n);
+int[] b=ArrayInfo(c);
+//int[] c=BinToDec(a);
+PrintArray(n, a, b, c);
+
+
+
+
+
+
 
 
